56d737d5dc11591e84f75718ace086b7971a7286
Tiago Luiz Seuaciuc de Poli <tiago.poli@stoneage.com.br>
2024-03-14 23:54:26 -0300

    chore: config tsconfig file

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import { App } from '@/App';
 import ReactDOM from 'react-dom/client';
-import { App } from './App.tsx';
+
 import './view/config/style/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
