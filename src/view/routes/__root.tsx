c1a9ecec48d85c2c2c07ec7eb7127c17226d0eef
Tiago Luiz Seuaciuc de Poli <tiago.poli@stoneage.com.br>
2024-03-21 23:57:16 -0300

    refactor: ensure to display only the Outlet if no user authenticated

--- src/view/routes/__root.tsx
+++ src/view/routes/__root.tsx
@@ -1,16 +1,25 @@
 import { Outlet, createRootRouteWithContext } from '@tanstack/react-router';
 
-import { FirebaseContextState } from '../providers/firebase';
+import { useFirebase, FirebaseContextState } from '../providers/firebase';
 import { MainLayout } from '../components/layout/main-layout/main-layout';
 
 type RouterContext = {
   firebaseContext: FirebaseContextState;
 };
 
+const Wrapper = () => {
+  const { context } = useFirebase();
+
+  if (context.userLoggedIn) {
+    return (
+      <MainLayout>
+        <Outlet />
+      </MainLayout>
+    );
+  }
+  return <Outlet />;
+};
+
 export const Route = createRootRouteWithContext<RouterContext>()({
-  component: () => (
-    <MainLayout>
-      <Outlet />
-    </MainLayout>
-  ),
+  component: () => <Wrapper />,
 });
