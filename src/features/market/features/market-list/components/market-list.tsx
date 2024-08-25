c0aa9939bb30362c8cead119e0ada733dbcfc909
Tiago Poli <tiagoluizpoli@gmail.com>
2024-08-25 02:27:25 -0300

    refactor: add an empty-market-list component

--- src/features/market/features/market-list/components/market-list.tsx
+++ src/features/market/features/market-list/components/market-list.tsx
@@ -1,15 +1,30 @@
 import { Pagination } from '@/components';
 import { useGetMarketListQuery } from '@/features/market/infrastructure';
 
+import NoDataSvg from '@/assets/images/adventure-map.svg';
+
 import { useMarketListContext } from '../market-list-context';
 import { MarketItem } from './market-item';
 
 export const MarketList = () => {
   const { paginationParams, setPaginationParams } = useMarketListContext();
-  const { data, status, isFetching } = useGetMarketListQuery(paginationParams);
+  const { data, status, isFetching, isLoading } = useGetMarketListQuery(paginationParams);
 
   if (status === 'error') {
     return <div>Error</div>;
+  }
+
+  if (isLoading) {
+    return <div>Carregando...</div>;
+  }
+
+  if (data && data.total === 0) {
+    return (
+      <div className="w-full flex flex-col gap-4 mt-8 justify-center items-center">
+        <img src={NoDataSvg} alt="No data" className="w-40" />
+        <h2>Nenhum mercado encontrado</h2>
+      </div>
+    );
   }
 
   return (
